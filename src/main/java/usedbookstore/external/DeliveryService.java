481a1d85cb91122ccbe8592701cc4bdfaee621d5
inho-oh <dhdlsgh111@naver.com>
2020-07-15 20:31:50 +0900

    3

--- src/main/java/usedbookstore/external/DeliveryService.java
+++ src/main/java/usedbookstore/external/DeliveryService.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//@FeignClient(name="Delivery", url="http://Delivery:8080")
-@FeignClient(name="Delivery", url="http://localhost:8085")
+@FeignClient(name="Delivery", url="http://Delivery:8080")
+//@FeignClient(name="Delivery", url="http://localhost:8085")
 public interface DeliveryService {
 
     @RequestMapping(method= RequestMethod.PUT, path="/deliveries/{id}")
